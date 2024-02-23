6187e7b58de28c0107091cdc7c0e9030f80d8e83
Blockland-crpto <73660072+Blockland-crpto@users.noreply.github.com>
2024-02-23 15:51:27 +0000

    updated help command

--- kernel/modules/shell/apps/help/help.c
+++ kernel/modules/shell/apps/help/help.c
@@ -4,8 +4,8 @@
 
 //Help main page
 void avaliable_commands(int page){
-  char helpstrpgo[235] ="Avaliable Commands\n\n`help` Shows this screen.\n\n`clear` Clears the screen\n\n`about` Shows information about the OS.\n\n`reboot` Reboots the computer.\n\n`cat <[Any] file>` Prints the contents of the file `file`.\n\nPress enter to continue";
-  char helpstrpgt[200] = "Avaliable Commands\n\n`ls` Prints a list of all the files and directories\n in the current directory.\n\n`shutdown` Shuts down the computer.\n\nPress enter to continue";
+  char helpstrpgo[235] ="Avaliable Commands\n\n`help` Shows this screen.\n\n`clear` Clears the screen\n\n`about` Shows information about the OS.\n\n`reboot` Reboots the computer.\n\n`cat <[Any] file>` Prints the contents of the file `file`.";
+  char helpstrpgt[200] = "Avaliable Commands\n\n`ls` Prints a list of all the files and directories\n in the current directory.\n\n`shutdown` Shuts down the computer.\n\n`calc` Opens a calculator.";
   if (page == 1) {
 	userinputmode = HELP_APP_ID;
 	make_gui(3, 3, 1, 1);
