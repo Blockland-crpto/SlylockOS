62b305260858fdbdeaf3638029567adefa3ada8e
Blockland-crpto <73660072+Blockland-crpto@users.noreply.github.com>
2024-02-26 12:30:44 +0000

    bootscreen modified to be quicker

--- kernel/modules/gui/func/bootscreen.c
+++ kernel/modules/gui/func/bootscreen.c
@@ -6,7 +6,7 @@ void bootscreen() {
 	make_gui(3, 3, 0, 0);
 	make_gui_windows("Starting Up", "MiniOS is starting...", 19, 10, 60, 20);
 	//gives a chance for the modules to initialize
-	loading_bar(35, 22, 15, 900000);
+	loading_bar(35, 22, 15, 500000);
 	//loading_bar_two(20, 22, 15);
 
 	//time_sleep(10000000);
