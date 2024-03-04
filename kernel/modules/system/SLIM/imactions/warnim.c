433339ddfe0e2221623a62fe9af60d9452579a04
Blockland-crpto <73660072+Blockland-crpto@users.noreply.github.com>
2024-03-04 21:51:56 +0000

    added the im for warn

---
+++ kernel/modules/system/SLIM/imactions/warnim.c
@@ -0,0 +1,16 @@
+#include <system/im.h>
+#include <slibaries/shell.h>
+#include <drivers/perip/keybrd.h>
+#include <slibaries/gui.h>
+#include <system/debug.h>
+
+void warnim(char c, int userinputmode) {
+	if (userinputmode == WARN_BOX_ID) {		
+		if (c == ENTER_KEY) {
+			tui_reset();
+		}
+	} else {
+		//report the error to end user
+		warning_box("IM Error", "Error occured in warnim, unexpected UIM");
+	}
+}
