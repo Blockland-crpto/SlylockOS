aef6af04fd25451c3cba3d8937170288cf08121f
Blockland-crpto <73660072+Blockland-crpto@users.noreply.github.com>
2024-03-20 21:34:32 +0000

    moved acpiCheckRSDPtr to its own file

---
+++ kernel/modules/drivers/acpi/helpers/acpiCheckRSDPtr.c
@@ -0,0 +1,27 @@
+#include <drivers/acpi.h>
+#include <string.h>
+#include <stddef.h>
+
+unsigned int *acpiCheckRSDPtr(uint32_t *ptr) {
+	const char *sig = "RSD PTR ";
+	struct RSDPtr *rsdp = (struct RSDPtr *) ptr;
+
+
+	if (memcmp(sig, rsdp, 8) == 0) {
+		unsigned char check = 0;
+		if (sizeof(struct RSDPtr) <= sizeof(*ptr)) {
+			unsigned char *bptr = (unsigned char *)rsdp;
+			for (size_t i = 0; i < 20; i++) {
+				check += *bptr;
+				bptr++;
+			}
+		}
+
+		// found valid rsdpd   
+		if (check == 0) {
+			return (unsigned int *) rsdp->RsdtAddress;
+		}
+	}
+
+	return NULL;
+}
