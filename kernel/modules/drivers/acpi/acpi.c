31b0398a0bb03202b8d0fd89d1e6b7a5a7955566
Blockland-crpto <73660072+Blockland-crpto@users.noreply.github.com>
2024-03-20 21:53:36 +0000

    move getrsdptr to its own file

--- kernel/modules/drivers/acpi/acpi.c
+++ kernel/modules/drivers/acpi/acpi.c
@@ -10,34 +10,6 @@
 
 
 
-unsigned int *acpiGetRSDPtr(void) {
-   unsigned int *addr;
-   unsigned int *rsdp;
-
-   // search below the 1mb mark for RSDP signature
-   for (addr = (uint32_t *) 0x000E0000; (int) addr<0x00100000; addr += 0x10/sizeof(addr))
-   {
-	  rsdp = acpiCheckRSDPtr(addr);
-	  if (rsdp != NULL)
-		 return rsdp;
-   }
-
-
-   // at address 0x40:0x0E is the RM segment of the ebda
-   int ebda = *((short *) 0x40E);   // get pointer
-   ebda = ebda*0x10 &0x000FFFFF;   // transform segment into linear address
-
-   // search Extended BIOS Data Area for the Root System Description Pointer signature
-   for (addr = (uint32_t *) ebda; (int) addr<ebda+1024; addr+= 0x10/sizeof(addr))
-   {
-	  rsdp = acpiCheckRSDPtr(addr);
-	  if (rsdp != NULL)
-		 return rsdp;
-   }
-
-   return NULL;
-}
-
 int acpiCheckHeader(uint32_t *ptr, char *sig) {
    if (memcmp(ptr, sig, 4) == 0)
    {
