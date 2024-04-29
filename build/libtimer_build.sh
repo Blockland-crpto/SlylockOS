cd70ac901960f67b2ba7e64161bd1e4ef9041d9f
Blockland-crpto <73660072+Blockland-crpto@users.noreply.github.com>
2024-04-29 18:04:51 +0000

    setup build system for C++

--- build/libtimer_build.sh
+++ build/libtimer_build.sh
@@ -13,6 +13,23 @@ objb="${objb} ${ta}"
 gcc -m32 -elf_i386 -Wall $optimize -fstrength-reduce -fomit-frame-pointer -fno-inline-functions -nostdinc -fno-builtin  -fstack-protector-all $debug $headers -c -o $ta $tb
 done
 
+cppsources=$(find ./libs/libtimer/* -type f -name "*.cpp")
+cppobjects=$(echo ${cppsources//\.cpp/.o})
+
+objbp=''
+charp=' '
+endp=$(awk -F"${charp}" '{print NF-1}' <<< "${cppobjects}")
+endp=$((endp+1))
+
+for i in $(seq 1 $endp); do 
+tap=$(echo ./libtimerbin/$(basename $(echo $cppobjects | cut -d" " -f$i )))
+tbp=$(echo $cppsources | cut -d" " -f$i)
+objbp="${objbp} ${tap}"
+g++ -m32 -elf_i386 $optimize $headers -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti -fpermissive -fstack-protector-all -c -o $tap $tbp
+
+done
+objbp="${objbp:1}"
+
 ar rcs lib/libtimer.a libtimerbin/*.o
 
 rm -r libtimerbin
--- main.sh
+++ main.sh
@@ -69,14 +69,35 @@ ta=$(echo ./bin/$(basename $(echo $cobjects | cut -d" " -f$i )))
 tb=$(echo $csources | cut -d" " -f$i)
 objb="${objb} ${ta}"
 gcc -m32 -elf_i386 -Wall $optimize -fstrength-reduce -fomit-frame-pointer -fno-inline-functions -nostdinc -fno-builtin $debug $headers -fstack-protector-all -c -o $ta $tb
+
 done
 objb="${objb:1}"
+
+cppsources=$(find ./kernel/modules/* -type f -name "*.cpp")
+cppobjects=$(echo ${cppsources//\.cpp/.o})
+
+objbp=''
+charp=' '
+endp=$(awk -F"${charp}" '{print NF-1}' <<< "${cppobjects}")
+endp=$((endp+1))
+
+for i in $(seq 1 $endp); do 
+tap=$(echo ./bin/$(basename $(echo $cppobjects | cut -d" " -f$i )))
+tbp=$(echo $cppsources | cut -d" " -f$i)
+objbp="${objbp} ${tap}"
+g++ -m32 -elf_i386 $optimize $headers -ffreestanding -O2 -Wall -Wextra -fno-exceptions -fno-rtti -fpermissive -fstack-protector-all -c -o $tap $tbp
+
+done
+objbp="${objbp:1}"
+
+
+
 
 
 gcc -m32 -elf_i386 -Wall $optimize -fstrength-reduce -fomit-frame-pointer -finline-functions -nostdinc -fno-builtin $debug $headers -fno-stack-protector  -c -o ./bin/main.o ./kernel/main.c
 
 export LD_LIBRARY_PATH=/home/runner/SlylockOS/
-ld -T link.ld --verbose -m elf_i386 -o kernel.bin ./bin/main.o $objb  ./bin/boot.o $libaries
+ld -T link.ld --verbose -m elf_i386 -o kernel.bin ./bin/main.o $objb $objbp ./bin/boot.o $libaries
 
 
 rm -r iso
